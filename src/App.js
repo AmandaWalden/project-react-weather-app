a11b62f46e4752defa1da2d84a282cef2f4a54c3
AmandaWalden <94139170+AmandaWalden@users.noreply.github.com>
2022-05-05 21:11:02 -0500

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://amandawalden.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Amanda Walden
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/AmandaWalden/project-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://amandawalden.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Amanda Walden
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/AmandaWalden/project-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
