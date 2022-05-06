b6792c534f08f7e8c391f57736ba91b5e5d48678
AmandaWalden <94139170+AmandaWalden@users.noreply.github.com>
2022-05-05 20:29:37 -0500

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,21 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://amandawalden.netlify.app/" target="_blank">
+          Amanda Walden
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/AmandaWalden/project-react-weather-app"
+          target="_blank">
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
