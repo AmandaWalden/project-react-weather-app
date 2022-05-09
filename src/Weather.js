7f7c0315481cf7020b14e8e92afa293d4aa984be
AmandaWalden <94139170+AmandaWalden@users.noreply.github.com>
2022-05-09 14:59:01 -0500

    styled .css

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,13 +28,19 @@ export default function Weather() {
         <li>Monday 2:00</li>
         <li>Mostly Sunny</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Sunny"
-          />
-          19℃
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Sunny"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">9</span>
+              <span className="unit">℃</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
@@ -39,7 +50,6 @@ export default function Weather() {
           </ul>
         </div>
       </div>
-      Hello from Weather
     </div>
   );
 }
