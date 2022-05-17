6edb5bc309e760aab305e1208f1439ddf75fff56
AmandaWalden <94139170+AmandaWalden@users.noreply.github.com>
2022-05-17 15:43:06 -0500

    added WeatherForecast's API

--- src/Weather.js
+++ src/Weather.js
@@ -8,8 +8,10 @@ export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
+    console.log(response.data)
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
@@ -58,7 +60,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
